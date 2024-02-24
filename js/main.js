717e9501709b471ae20baef5242572a988686083
Sergey Nikiforov <nsv-92@mail.ru>
2024-02-24 16:35:27 +0300

    верстка и стилизация формы

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// document.querySelector(".contact-form").addEventListener("submit", function(event) {
+//     const checkbox = document.querySelector(".real-checkbox");
+//     if(!checkbox.checked) {
+//         event.preventDefault(); // Отменить отправку формы
+//         checkbox.style.color = "red"; // Изменить цвет текста на красный
+//     }
+// })
