e4a0112177180401751047db0d57c8274c12aee8
Sergey Nikiforov <nsv-92@mail.ru>
2024-02-29 19:19:25 +0300

    валидация формы

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// Слайдер Swiper
+
 var swiper = new Swiper(".slider", {
     pagination: {
       el: ".swiper-pagination",
@@ -23,3 +25,40 @@ var swiper = new Swiper(".slider", {
         disableOninteraction: false
     }
   });
+
+  // Маска для телефона inputmask 
+let selector = document.querySelectorAll('input[type="tel"]');
+let im = new Inputmask('+7 (999) 999-99-99');
+im.mask(selector);
+
+// валидация форм
+let validateforms = function(selector, rules, successModal, yaGoal) {
+  new window.JustValidate(selector, {
+    rules: rules,
+    submitHandler: function(form) {
+      let formData = new FormData(form);
+      let xhr = new XMLHttpRequest();
+
+      zhr.onreadystatechage = function() {
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            console.log ('Отправлено');
+          }
+        }
+      }
+
+      xhr.open('POST', 'php/mail.php', true);
+      xhr.send(formData);
+
+      form.reset();
+    }
+  });
+}
+
+validateforms('.contact-form', {
+  email: {required: true, email: true}, 
+  tel: {required: true}, 
+  name:{required: true, minLength: 2, maxLength: 15},
+  checkbox: {required: true}
+})
+
